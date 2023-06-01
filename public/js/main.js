17f22eb18c553dbf3edc3e2a8231e8e26b23c793
boonruang <scubatoy@gmail.com>
2023-06-01 23:09:51 +0700

    30. State management - adding our custom store

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
+import * as store from './store.js'
+
 const socket = io('/')
 
 socket.on('connect', () => {
   console.log('successfully connected to socket.io server')
-  console.log(socket.id)
+  store.setSocketId(socket.id)
 })
