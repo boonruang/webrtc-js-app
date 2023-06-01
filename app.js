12d1a17677046c3821987820cc868c9b3b229903
boonruang <scubatoy@gmail.com>
2023-06-01 13:45:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const http = require('http')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('User connected to socket.IO server')
+  console.log(socket.id)
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
