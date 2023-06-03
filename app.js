6b975709a1bfe63d7aafa540fc0bb915654104b5
boonruang <scubatoy@gmail.com>
2023-06-03 22:48:15 +0700

    40. Solving problem with no request coming

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected')
 
-    const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
-      peerSocketId !== socket.id
-    })
+    const newConnectedPeers = connectedPeers.filter(
+      (peerSocketId) => peerSocketId !== socket.id
+    )
 
     connectedPeers = newConnectedPeers
     console.log('disconnect', connectedPeers)
